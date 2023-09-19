d4f8c7868e047ddf9011dd066df9e53f66aa0b87
Dell-User <Dell-User@DESKTOP-4LJVQAT>
2023-09-19 04:26:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Static_Methods_and_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Sqrt(144));
+
+
+            SayHi.sayHello("Nokubongwa");
+
+            
+
+            //freeze
+            Console.Read();
+        }
+    }
+}
---
+++ SayHi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Static_Methods_and_Classes
+{
+    class SayHi
+    {
+        public static void sayHello(string name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
